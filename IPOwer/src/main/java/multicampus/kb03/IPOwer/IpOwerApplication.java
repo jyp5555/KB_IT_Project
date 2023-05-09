dda7cadc6d2b204ec8cf9d3670d584e73996e1ee
jyp5555 <jyp2095@sookmyung.ac.kr>
2023-05-09 17:13:54 +0900

    StartProject

---
+++ IPOwer/src/main/java/multicampus/kb03/IPOwer/IpOwerApplication.java
@@ -0,0 +1,13 @@
+package multicampus.kb03.IPOwer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpOwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpOwerApplication.class, args);
+	}
+
+}
